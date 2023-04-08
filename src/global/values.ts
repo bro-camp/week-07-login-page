ea8efd8eee09585d9b013b3579c3939f92003bc9
I-_-I <>
2023-04-08 14:25:13 +0530

    Use dbUrl and PORT from environment variables

--- src/global/values.ts
+++ src/global/values.ts
@@ -1,2 +1,2 @@
-export const dbUrl = 'mongodb://127.0.0.1:27017/temp';
-export const PORT = 8080;
+export const dbUrl = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/temp';
+export const PORT = process.env.PORT || 8080;
